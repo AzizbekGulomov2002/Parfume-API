c3d4b179d101363c18d8e17d29355ff34b479bc7
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2024-10-02 22:46:27 +0500

    [UPDATED] API

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&m*b&*7ri7)0rjgsrh0w0a1$m377ce$2w)377#ny_+d-ton4p@'
 DEBUG = True
 import os
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
